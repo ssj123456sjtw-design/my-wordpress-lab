6416504dd8266183129072edf1b38e9d70ae2c6e
ssj123456sjtw-design <ssj123456sjtw@gmail.com>
2026-07-25 17:24:58 +0800

    change web text

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-    res.end('自動化部署網頁成功！');
+    res.end('這是第一次全自動化部署測試，成功啦！！');
 });
 server.listen(3000, () => {
     console.log('Server is running on port 3000');
